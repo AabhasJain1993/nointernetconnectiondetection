88c0208ae1718081aff2649d567912ce3d51a469
Marco Uberti <ube.labs@gmail.com>
2015-06-27 11:28:35 +0200

    First commit.

---
+++ NoInternetConnectionDetection/app/src/main/java/com/ubelab/nointernetconnectiondetection/MainActivity.java
@@ -0,0 +1,33 @@
+package com.ubelab.nointernetconnectiondetection;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+
+public class MainActivity extends BaseActivity {
+
+    Button checkConnectivityBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        checkConnectivityBtn = (Button)findViewById(R.id.checkConnectivityBtn);
+        checkConnectivityBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(ConnectionHelper.isConnectedOrConnecting(getApplicationContext())) {
+                    hideErrorsBar(true);
+                }else {
+                    hideErrorsBar(false);
+                }
+            }
+        });
+    }
+
+}
